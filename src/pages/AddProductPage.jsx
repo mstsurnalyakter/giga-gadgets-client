8e2c80fd3616f0a593e6ee564b86d52a7154dda8
mstsurnalyakter <surnalyms@gmail.com>
2024-04-24 15:34:04 +0600

    added product functionally

--- src/pages/AddProductPage.jsx
+++ src/pages/AddProductPage.jsx
@@ -4,34 +4,51 @@ import toast from "react-hot-toast";
 import Spinner from "../components/Spinner.jsx";
 import { useLoaderData } from "react-router-dom";
 import useAuth from "../hooks/useAuth.jsx";
+import Swal from "sweetalert2";
 
 // import baseURL from '../utilitis/url.js'
 const AddProductPage = ({ update }) => {
+
   const { user } = useAuth() || {};
+
   const handleAddProduct = (e) => {
     e.preventDefault();
-     
 
-    const name = e.target.name.value;
-    const price = e.target.price.value;
-    const image = e.target.image.value;
-    const type = e.target.type.value;
+    const form = e.target;
+
+
+    const name = form.name.value;
+    const price = form.price.value;
+    const image = form.image.value;
+    const type = form.type.value;
+    const rating = form.rating.value;
+    const brand = form.brand.value;
+
     const email = user.email;
 
     // console.log(name, price, image, type)
 
-    const info = { name, price, image, type, email };
+    const info = { name, price, image, type, email,rating,brand };
+    console.log(info);
 
     fetch("http://localhost:5000/addProduct", {
       method: "POST",
-      headers: { "Content-type": "application/json" },
+      headers: { "Content-Type": "application/json" },
       body:JSON.stringify(info)
     })
       .then(res => res.json())
       .then(data => {
-        if (data?.insertedId) {
-        alert("bhai data insert hoice")
+
+      if (data.insertedId) {
+        Swal.fire({
+          title: "Success!",
+          text: "Product Added Successfully",
+          icon: "success",
+          confirmButtonText: "Cool",
+        });
+        form.reset();
       }
+
     })
 
   };
