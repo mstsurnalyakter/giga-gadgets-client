21402bb70e31fd29a7667af4497213a635340da3
mstsurnalyakter <surnalyms@gmail.com>
2024-04-24 16:13:30 +0600

    firebase setup

--- src/config/firebase.config.jsx
+++ src/config/firebase.config.jsx
@@ -2,20 +2,21 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const ENV = import.meta.env
-// Your web app's Firebase configuration
+const ENV = import.meta.env;
+
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCoZ-Z6ZOet4DGjL--eAijj7LtjIVKOYw0",
-    authDomain: "testfirebase-871ac.firebaseapp.com",
-    projectId: "testfirebase-871ac",
-    storageBucket: "testfirebase-871ac.appspot.com",
-    messagingSenderId: "487491252835",
-    appId: "1:487491252835:web:aed666f03a72f805a53e56"
-  }
-
-
+  apiKey:ENV.VITE_APIKEY,
+  authDomain:ENV.VITE_AUTHDOMAIN,
+  projectId:ENV.VITE_PROJECTID,
+  storageBucket:ENV.VITE_STORAGEBUCKET,
+  messagingSenderId:ENV.VITE_MESSAGINGSENDERID,
+  appId:ENV.VITE_APPID,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+
 const auth = getAuth(app);
 export default auth;
