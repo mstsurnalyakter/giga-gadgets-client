fe726ab91acc7e2e388ea3b4d785203c6555042a
mstsurnalyakter <surnalyms@gmail.com>
2024-04-24 16:35:47 +0600

    firebase file updated

--- src/config/firebase.config.jsx
+++ src/config/firebase.config.jsx
@@ -4,19 +4,17 @@ import { getAuth } from "firebase/auth";
 
 const ENV = import.meta.env;
 
-
 const firebaseConfig = {
-  apiKey:ENV.VITE_APIKEY,
-  authDomain:ENV.VITE_AUTHDOMAIN,
-  projectId:ENV.VITE_PROJECTID,
-  storageBucket:ENV.VITE_STORAGEBUCKET,
-  messagingSenderId:ENV.VITE_MESSAGINGSENDERID,
-  appId:ENV.VITE_APPID,
+  apiKey: ENV.VITE_APIKEY,
+  authDomain: ENV.VITE_AUTHDOMAIN,
+  projectId: ENV.VITE_PROJECTID,
+  storageBucket: ENV.VITE_STORAGEBUCKET,
+  messagingSenderId: ENV.VITE_MESSAGINGSENDERID,
+  appId: ENV.VITE_APPID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 
 const auth = getAuth(app);
 export default auth;
